9b8af7691da4f9f1416bf3fef2d23cb5d390b0a3
fg4cc <>
2024-02-29 04:59:50 +0100

    Improve export error detection

--- pes-model/IO.py
+++ pes-model/IO.py
@@ -722,4 +722,15 @@ def exportModel(context, rootObjectName):
 	model.meshes = meshes
 	model.boundingBox = boundingBox
 	
+	errors = []
+	for mesh in model.meshes:
+		if len(mesh.vertices) > 65535:
+			errors.append("Mesh '%s' contains %s vertices out of a maximum of 65535" % (meshName, len(mesh.vertices)))
+		#if len(mesh.faces) > 21845:
+		#	errors.append("Mesh '%s' contains %s faces out of a maximum of 21845" % (meshName, len(mesh.faces)))
+		#if mesh.boneGroup is not None and len(mesh.boneGroup.bones) > 32:
+		#	errors.append("Mesh '%s' bone group contains %s bones out of a maximum of 32" % (meshName, len(mesh.boneGroup.bones)))
+	if len(errors) > 0:
+		raise ExportError(errors)
+	
 	return model
